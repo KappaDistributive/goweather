a50cf03af3d6b5a4bfd1098f0c799c0258edc081
Stefan Mesken <stefan.mesken@deepl.com>
2022-08-26 09:57:52 +0200

    refactoring

--- v1/main.go
+++ v1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,8 +12,53 @@ import (
 	"strings"
 )
 
+var logger = log.New(os.Stderr, "", 0)
+
+func getWeather(city string, provider string, api_key string, format string) (WeatherData, error) {
+	if strings.HasPrefix(provider, "wttr") {
+		resp, err := http.Get(fmt.Sprintf("https://wttr.in/%s?format=j1", city))
+		if err != nil {
+			logger.Println(err)
+			return WeatherData{}, err
+		} else if resp.StatusCode != 200 {
+			return WeatherData{}, errors.New(fmt.Sprintf("Status code %d", resp.StatusCode))
+		} else {
+			body, err := ioutil.ReadAll(resp.Body)
+			defer resp.Body.Close()
+			if err != nil {
+				return WeatherData{}, err
+			}
+			var data WttrPayload
+			err = json.Unmarshal(body, &data)
+			if err != nil {
+				return WeatherData{}, err
+			}
+			return data.createWeather(format), nil
+		}
+	} else if strings.HasPrefix(provider, "openweather") {
+		resp, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, api_key))
+		if err != nil {
+			return WeatherData{}, err
+		} else if resp.StatusCode != 200 {
+			return WeatherData{}, errors.New(fmt.Sprintf("Status code %d", resp.StatusCode))
+		} else {
+			body, err := ioutil.ReadAll(resp.Body)
+			defer resp.Body.Close()
+			if err != nil {
+				return WeatherData{}, err
+			}
+			var data OpenWeatherPayload
+			err = json.Unmarshal(body, &data)
+			if err != nil {
+				return WeatherData{}, err
+			}
+			return data.createWeather(format), nil
+		}
+	}
+	return WeatherData{}, errors.New("Reached end of function without return. This should never happen.")
+}
+
 func main() {
-	logger := log.New(os.Stderr, "", 0)
 
 	var city string
 	var weather_method string
@@ -24,49 +70,10 @@ func main() {
 	flag.StringVar(&api_key, "api_key", "n/a", "API key.")
 	flag.Parse()
 
-	if strings.HasPrefix(weather_method, "wttr") {
-		resp, err := http.Get("https://wttr.in/" + city + "?format=j1")
-		if err != nil {
-			logger.Println(err)
-			os.Exit(1)
-		} else if resp.StatusCode != 200 {
-			logger.Printf("Status code %d", resp.StatusCode)
-			os.Exit(1)
-		} else {
-			body, err := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			if err != nil {
-				logger.Println(err)
-				os.Exit(1)
-			}
-			var data WttrPayload
-			err = json.Unmarshal(body, &data)
-			if err != nil {
-				logger.Println(err)
-				os.Exit(1)
-			}
-			weather := data.createWeather(format)
-			fmt.Println(weather)
-		}
-	} else if strings.HasPrefix(weather_method, "openweather") {
-		resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
-		if err != nil {
-			logger.Println(err)
-			os.Exit(1)
-		} else if resp.StatusCode != 200 {
-			logger.Printf("Status code %d", resp.StatusCode)
-			os.Exit(1)
-		} else {
-			body, err := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			if err != nil {
-				logger.Println(err)
-				os.Exit(1)
-			}
-			var data OpenWeatherPayload
-			err = json.Unmarshal(body, &data)
-			weather := data.createWeather(format)
-			fmt.Println(weather)
-		}
+	weather, err := getWeather(city, weather_method, api_key, format)
+	if err != nil {
+		logger.Fatal(err)
+		os.Exit(1)
 	}
+	fmt.Println(weather)
 }
